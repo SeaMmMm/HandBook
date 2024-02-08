1fa841dfdaf64fbd89ca357aa180b17ae06427c5
Seaming <347552878@qq.com>
2024-02-08 18:26:12 +0800

    js

--- code/javascript笔记/03.数据类型/13.Json.js
+++ code/javascript笔记/03.数据类型/13.Json.js
@@ -212,3 +212,75 @@ console.log(schedule.meetups[1].date.getDate())
 - 这两种方法都支持用于智能读/写的转换函数。
 - 如果一个对象具有 toJSON，那么它会被 JSON.stringify 调用。
 */
+
+// 任务
+
+/* 
+将对象转换为 JSON，然后再转换回来
+重要程度: 5
+将 user 转换为 JSON，然后将其转换回到另一个变量。
+
+let user = {
+  name: "John Smith",
+  age: 35
+};
+*/
+
+json = JSON.stringify(user)
+console.log(json)
+console.log(JSON.parse(json))
+
+/* 
+排除反向引用
+重要程度: 5
+在简单循环引用的情况下，我们可以通过名称排除序列化中违规的属性。
+
+但是，有时我们不能只使用名称，因为它既可能在循环引用中也可能在常规属性中使用。因此，我们可以通过属性值来检查属性。
+
+编写 replacer 函数，移除引用 meetup 的属性，并将其他所有属性序列化：
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  occupiedBy: [{name: "John"}, {name: "Alice"}],
+  place: room
+};
+
+// 循环引用
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+
+}));
+
+// 结果应该是：
+{
+  "title":"Conference",
+  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
+  "place":{"number":23}
+}
+*/
+
+room = {
+  number: 23,
+}
+
+meetup = {
+  title: 'Conference',
+  occupiedBy: [{ name: 'John' }, { name: 'Alice' }],
+  place: room,
+}
+
+room.occupiedBy = meetup
+meetup.self = meetup
+
+console.log(
+  JSON.stringify(meetup, function replacer(key, value) {
+    return key != '' && value == meetup ? undefined : value
+  })
+)
+// 这里我们还需要判断 key=="" 以排除第一个调用时 value 是 meetup 的情况。
