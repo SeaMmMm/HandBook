2dafed41d01275881575587fefec65199fec156e
Seaming <347552878@qq.com>
2024-03-19 23:00:15 +0800

    Proxy
    TODO: Reflect

--- code/javascript笔记/TEST.js
+++ code/javascript笔记/TEST.js
@@ -1,16 +1,66 @@
-class User {
-  constructor(name) {
-    this.name = name
-  }
+'use strict'
+console.log(new Number(2) instanceof Number)
+console.log(2 instanceof Number)
+console.log(typeof 2)
 
-  sayHi() {
-    console.log(`Hello ${this.name}`)
+console.log((2).constructor === Number)
+console.log(true.constructor === Boolean)
+
+function Fn() {}
+Fn.prototype = new Array()
+var f = new Fn()
+console.log(f.constructor === Fn)
+console.log(f.constructor === Array)
+
+const a = Object.prototype.toString
+console.log(a.call(1))
+console.log(a.call(new Array(2)))
+console.log(a.call([]))
+console.log(a.call(Symbol()))
+
+console.log(null == undefined)
+console.log(null === undefined)
+
+function myInstanceof(left, right) {
+  let proto = Object.getPrototypeOf(left)
+  let prototype = right.prototype
+
+  while (true) {
+    if (!proto) return false
+    if (proto === prototype) return true
+
+    proto = Object.getPrototypeOf(proto)
   }
 }
 
-console.log(typeof User)
-console.log(User === User.prototype.constructor)
-console.log(User.prototype)
-console.log(new User('Jack').__proto__ === User.prototype)
-console.log(User.prototype.sayHi)
-console.log(Object.getOwnPropertyNames(User.prototype))
+function getStudent(student) {
+  console.log(this.title + ': ' + student)
+}
+
+let group = {
+  title: 'Our Group',
+  students: ['John', 'Pete', 'Alice'],
+
+  showList() {
+    this.students.forEach(
+      function getStudents(student) {
+        console.log(this.title + ': ' + student)
+      }.bind(this)
+    )
+  },
+}
+group.showList()
+
+let onWatch = (obj, setBind, getLogger) => {
+  let handler = {
+    get(target, property, receiver) {
+      getLogger(target, property)
+      return Reflect.get(target, property, receiver)
+    },
+    set(target, property, value, recevier) {
+      setBind(value, property)
+      return Reflect.set(target, property, value)
+    },
+  }
+  return new Proxy(obj, handler)
+}
