138870168d04afbc0f797ad2b3bdd17efbad1902
IIIIIIIIIIIII <87215099+SeaMmMm@users.noreply.github.com>
2024-04-13 18:03:51 +0800

    Revert "1.5 浏览器内置 js 对象 、事件模型 、请求相关内容详解"

--- javascript笔记/TEST.js
+++ javascript笔记/TEST.js
@@ -1,27 +1,28 @@
-const fs = require('fs').promises
-const path = require('path')
+// 同步写法完成异步方法
 
-// 指定要读取的文件夹路径
-const folderPath =
-  '/Users/smc/Downloads/毕设/软件开发用训练图/1：1 10×典型图，易区分'
+const sleep = (a) => a
 
-async function processFiles() {
-  try {
-    const files = await fs.readdir(folderPath)
+function* func() {
+  const res1 = yield sleep(1)
+  console.log(res1)
 
-    for (let file of files) {
-      if (!file.endsWith('txt')) continue
+  const res2 = yield sleep(2)
+  console.log(res2)
 
-      const filePath = path.join(folderPath, file)
-      const content = await fs.readFile(filePath, 'utf8')
-      console.log(content)
+  const res3 = yield sleep(3)
+  console.log(res3)
 
-      // 可以在这里对文件内容进行进一步的处理
-    }
-    console.log('All files have been processed successfully.')
-  } catch (err) {
-    console.error('An error occurred:', err)
-  }
+  return
 }
 
-processFiles()
+function run(gen) {
+  const g = gen()
+
+  function next(data) {
+    const result = g.next(data)
+    if (result.done) return
+    result.value.then((data) => next(data))
+  }
+
+  next()
+}
